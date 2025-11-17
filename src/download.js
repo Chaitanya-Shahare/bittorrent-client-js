97b54c3968677b4d2bde1b32d90532d450742fc2
Chaitanya Shahare <shaharechaitanya3@gmail.com>
2025-11-17 09:59:36 +0530

    updating download.js

--- src/download.js
+++ src/download.js
@@ -1,16 +1,38 @@
 import net from "net";
 import { Buffer } from "buffer";
-import tracker from "./src/tracker";
+import tracker from "./tracker.js";
 
 const download = (peer) => {
+  console.log(peer);
   const socket = net.Socket();
   socket.on("error", console.log);
 
-  socket.connect(peer.port, peer.ip, () => {
+  socket.connect(peer.port, peer.ip, (msg) => {
     // socket.write(...) write a message here
+    console.log("msg", msg);
   });
-  socket.on("data", (data) => {
+
+  onWholeMsg(socket, (data) => {
     // handle response here
+  });
+};
+
+const onWholeMsg = (socket, callback) => {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+
+  socket.on("data", (recvBuf) => {
+    // msgLen calculates the length of a whole message
+    //
+    const msgLen = () =>
+      handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
   });
 };
 
