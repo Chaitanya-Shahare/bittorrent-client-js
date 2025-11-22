91271bc803586a6f890ef5994404c669fd20c7d9
Chaitanya Shahare <shaharechaitanya3@gmail.com>
2025-11-22 19:11:38 +0530

    add non-handshake message handlers

--- src/download.js
+++ src/download.js
@@ -16,11 +16,39 @@ const download = (peer, torrent) => {
   onWholeMsg(socket, (msg) => msgHandler(msg, socket));
 };
 
-const msgHandler = (msg, socket) => {
+function msgHandler(msg, socket) {
   if (isHandshake(msg)) {
     socket.write(message.buildInterested());
+  } else {
+    const m = message.parse(msg);
+
+    if (m.id === 0) chokeHandler();
+    if (m.id === 1) unchokeHandler();
+    if (m.id === 4) haveHandler(m.payload);
+    if (m.id === 5) bitfieldHandler(m.payload);
+    if (m.id === 7) pieceHandler(m.payload);
   }
-};
+}
+
+function chokeHandler() {
+  //...
+}
+
+function unchokeHandler() {
+  //...
+}
+
+function haveHandler(payload) {
+  //...
+}
+
+function bitfieldHandler(payload) {
+  //...
+}
+
+function pieceHandler(payload) {
+  //...
+}
 
 const isHandshake = (msg) => {
   return (
